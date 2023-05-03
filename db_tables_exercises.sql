065a38f02299151c0f0278d12db4625be71bd60f
alejandrocastorena <alejandro.f.castorena@gmail.com>
2023-05-03 16:56:32 -0500

    Made an oppsie and fixed it

---
+++ db_tables_exercises.sql
@@ -0,0 +1,28 @@
+SHOW DATABASES;
+
+USE albums_db;
+
+SELECT database();
+
+SHOW TABLES;
+
+-- INT, FLOATS, CHAR, VARCHAR, TEXT, and DATES
+
+USE employees;
+
+SELECT database();
+
+SHOW tables;
+
+SHOW CREATE database employees;
+
+-- all of them contain a numeric type column
+-- departmenys, dept_emp, dept_manager, employees, title
+-- dept_emp, dept_manager, employees, salaries, title
+ -- there is no direct connect between the two tables
+ 
+ SHOW CREATE TABLE dept_manager;
+
+
+
+
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -13,5 +13,4 @@ SELECT *
 FROM employees
 WHERE hire_date LIKE '199%'
 ORDER BY hire_date 
-LIMIT 5 OFFSET 25;
-
+LIMIT 5 OFFSET 45;
